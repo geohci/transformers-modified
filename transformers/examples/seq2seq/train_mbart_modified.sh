3ce9681de3849831af3375215d51b7464946b854
msakota <52159049+msakota@users.noreply.github.com>
2021-03-15 10:28:55 +0100

    Add script

---
+++ transformers/examples/seq2seq/train_mbart_modified.sh
@@ -0,0 +1,18 @@
+python finetune_trainer.py \
+    --learning_rate=3e-5 \
+    --do_train \
+    --freeze_encoder \
+    --freeze_embeds \
+    --n_val 1000 \
+    --max_source_length 512 \
+    --per_device_train_batch_size=1 \
+    --per_device_eval_batch_size=1 \
+    --num_train_epochs 5 \
+    --dataloader_num_workers 1 \
+    --tgt_lang en_XX \
+    --src_lang en_XX \
+    --save_steps 10000 \
+    --tokenizer_name facebook/mbart-large-cc25 \
+    --model_name_or_path facebook/mbart-large-cc25 \
+    --languages en_XX,cs_CZ,fr_XX,it_IT,fi_FI,de_DE,nl_XX,ro_RO,ru_RU,ja_XX \
+    "$@"
