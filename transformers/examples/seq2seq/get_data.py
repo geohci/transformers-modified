c34e571f3bd454aa183eeadb36fdff83c305c0f2
msakota <52159049+msakota@users.noreply.github.com>
2021-06-03 11:54:50 +0200

    Update get_data.py

--- transformers/examples/seq2seq/get_data.py
+++ transformers/examples/seq2seq/get_data.py
@@ -20,7 +20,7 @@ parser.add_argument("--graph_path", type=str, help="path to wikidata graph relat
 
 args, uknown = parser.parse_known_args()
 
-if os.path.exists(args.data_dir) and len(os.path.listdir(args.datadir) != 0):
+if os.path.exists(args.data_dir) and len(os.listdir(args.datadir) != 0):
     print("Data already loaded")
     quit()
 
