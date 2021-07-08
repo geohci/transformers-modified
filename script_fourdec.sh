429c1ebf7c8b59476fe07f320607bc9ac09a5cde
msakota <52159049+msakota@users.noreply.github.com>
2021-07-08 14:05:41 +0200

    Update script_fourdec.sh

--- script_fourdec.sh
+++ script_fourdec.sh
@@ -1,6 +1,6 @@
 python ./transformers/examples/seq2seq/get_data_fourdec.py \
-    --languages en_XX,cs_CZ,fr_XX,it_IT,fi_FI,de_DE,nl_XX,ro_RO,ru_RU,ja_XX \
-    --samples_train 100000 \
+    --languages en_XX,cs_CZ,fr_XX,it_IT,fi_FI,de_DE,nl_XX,ro_RO,ru_RU,ja_XX,ar_AR,tr_TR,lt_LT,lv_LV,es_XX,et_EE,ko_KR,gu_IN,kk_KZ,my_MM,si_LK,vi_VN,zh_CN,ne_NP,hi_IN \
+    --samples_train 200000 \
     --samples_val 10000 \
     --samples_test 10000 \
     --path /content/gdrive/MyDrive/wikidata_folders/ \
@@ -28,10 +28,10 @@ python ./transformers/examples/seq2seq/run_summarization.py \
     --model_name_or_path facebook/mbart-large-cc25 \
     --bert_path bert-base-multilingual-uncased \
     --fourdecoders \
-    --languages en_XX,cs_CZ,fr_XX,it_IT,fi_FI,de_DE,nl_XX,ro_RO,ru_RU,ja_XX \
+    --languages en_XX,cs_CZ,fr_XX,it_IT,fi_FI,de_DE,nl_XX,ro_RO,ru_RU,ja_XX,ar_AR,tr_TR,lt_LT,lv_LV,es_XX,et_EE,ko_KR,gu_IN,kk_KZ,my_MM,si_LK,vi_VN,zh_CN,ne_NP,hi_IN \
     "$@"
 python test_model.py \
-	--languages en_XX,cs_CZ,fr_XX,it_IT,fi_FI,de_DE,nl_XX,ro_RO,ru_RU,ja_XX \
+	--languages en_XX,cs_CZ,fr_XX,it_IT,fi_FI,de_DE,nl_XX,ro_RO,ru_RU,ja_XX,ar_AR,tr_TR,lt_LT,lv_LV,es_XX,et_EE,ko_KR,gu_IN,kk_KZ,my_MM,si_LK,vi_VN,zh_CN,ne_NP,hi_IN \
     --fourdecoders \
     --bert_path bert-base-multilingual-uncased \
     "$@"
